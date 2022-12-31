c9a51f5f127d81efeb9051851be825fb07778ad9
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-12-31 09:37:54 +0530

    Initialized the project and created the backend server.

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Application listening on http://localhost:${port}`)
+})
