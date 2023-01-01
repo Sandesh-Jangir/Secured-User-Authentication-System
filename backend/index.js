13576a8883f455f488bdcb42191c56478e0fb83f
Sandesh Jangir <sandeshatgithub@gmail.com>
2023-01-01 12:35:28 +0530

    Created the endpoint for signup and added the requested user to database.

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const app = express()
 const connectToMongo = require("./database")
+const User = require("./UserSchema")
 const port = 8000
 
 // Connecting To DataBase
 connectToMongo();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// To parse request json
+app.use(express.json())
+
+app.get('/api/signup', (req, res) => {
+
+  // Creating a new user
+  const fetchedUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    work: req.body.work
+  })
+  // Saving the user in the database.
+  fetchedUser.save();
+  res.json(fetchedUser) // response.
 })
 
 app.listen(port, () => {
