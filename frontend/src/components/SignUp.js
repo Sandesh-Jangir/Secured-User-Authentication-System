8ad33b4f85235303849277b8d61bc1b0b1d2d91b
Sandesh Jangir <sandeshatgithub@gmail.com>
2023-01-05 15:24:18 +0530

    Designed the signup component and made it fully functionable.

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,10 +1,58 @@
-import React from 'react'
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "../styles/main.css"
+import "../styles/signup.css"
 
 const SignUp = () => {
+  let navigate = useNavigate()// React router hook used for redirecting.
+  // Function to handle the submit.
+  const handleSubmit = async(e)=>{
+    // Preventing the default reload on form submit.
+    e.preventDefault();
+  
+    // Fetching the input feilds.
+    const name = document.getElementById("name").value
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    const work = document.getElementById("work").value
+  
+    // Forming the jsonified data to send to the api.
+    const user = JSON.stringify({
+      name: name,
+      email: email,
+      password: password,
+      work: work,
+    });
+    const parsedUser = JSON.parse(user)
+    
+    // Creating the user with the help of the backend api.
+    try {
+      // Sending the request.
+      const request = await axios.post("http://localhost:8000/api/auth/signup", parsedUser)
+      const data = request['data']; // reponse data
+      // If user is created with no errors then add the auth-token to the local storage.
+      if(data["success"]){
+        localStorage.setItem("authtoken", data["authToken"])
+        navigate("/dashboard") // Redirect to dashboard component (which we'll create later).
+      }
+    } catch (error) { // For anonymous errors.
+      console.log(error)
+    }
+  }
   return (
-    <>
-        <h1>This is signup component</h1>
-    </>
+    <div className='form-container'>
+      <div className="side">
+        <h2>Thank You </h2>
+        <h2>For Joining Us!</h2>
+      </div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" id="name" placeholder='name' required/>
+          <input type="email" id="email" placeholder='unique@email' required/>
+          <input type="password" id="password" placeholder='password' required/>
+          <input type="text" id="work" placeholder='work' required/>
+          <button className="btn-form" type='submit'>Create Account</button>
+        </form>
+    </div>
   )
 }
 
