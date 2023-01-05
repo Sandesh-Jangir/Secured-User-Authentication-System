df72e62076ca5272b529db6576f3cf1664736b50
Sandesh Jangir <sandeshatgithub@gmail.com>
2023-01-05 14:28:27 +0530

    Did the required configureations to connect the frontend and the backend.

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,13 @@ const connectToMongo = require("./database");
 
 const port = 8000;
 
+// Protecting the fetch request to get blocked by CORS.
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // Connecting To DataBase
 connectToMongo();
 
