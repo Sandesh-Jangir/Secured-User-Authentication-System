b992b4cec1f3a17baf1cc6b234c2593478613419
Sandesh Jangir <sandeshatgithub@gmail.com>
2023-01-06 12:42:36 +0530

    Designed the Login component and made it fully functionable. Also fixed a css bug in home.css.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import "../styles/main.css";
+import "../styles/login.css";
+import axios from "axios";
+import {useNavigate} from "react-router-dom"
 
 const Login = () => {
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    // Preventing from default reload on submit.
+    e.preventDefault();
+    // Fetching the entered feilds.
+    const data = JSON.stringify({
+      email: document.getElementById("email").value,
+      password: document.getElementById("password").value
+    });
+    // Parsing the data to JSON.
+    const parsedData = JSON.parse(data);
+
+    try {
+      // Sending the post request to the api.
+      const request = await axios.post("http://localhost:8000/api/auth/login", parsedData);
+      const response = request["data"] // Filtering the required data.
+      if (response["success"]){ // If found user in db.
+        localStorage.setItem("authToken", response["authToken"])
+        navigate("/dashboard") // Redirect to dashboard component.
+      }
+    } catch (error) { // For anonymous errors.
+      console.log(error)
+    }
+  };
   return (
     <>
-        <h1>This is login component.</h1>
+      <h1>We're Glag Seeing You Back</h1>
+      <form onSubmit={handleSubmit} className="login-form">
+        <input type="email" id="email" placeholder="authorized@email" />
+        <input type="password" id="password" placeholder="password" />
+        <button type="submit">Get Back In</button>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
